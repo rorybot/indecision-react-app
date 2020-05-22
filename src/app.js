71ed75c9d2917dbb4984243b3aba7898b07c2462
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-05-22 16:00:44 -0700

    Basic prop passing and rendering structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,70 @@
+class IndecisionApp extends React.Component {
+  render(){
+    const title = "Indecision App";
+    const subtitle = "What're you gonna do?";
+    const options = ['Eat Chocolate', 'Make pie', 'Clean room'];
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle}/>
+        <Action/>
+        <Options options={options} />
+        <AddOption/>
+      </div>
+    )
+  }
+}
+class Header extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    )
+  }
+}
+
+class Action extends React.Component {
+  render(){
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    )
+  }
+}
+
+class Options extends React.Component {
+  render(){
+    return (
+      <div>
+        {this.props.options.length}
+        {this.props.options.map( option => <Option key={option} optionText={option} /> )}
+      </div>
+    )
+  }
+}
+
+class AddOption extends React.Component {
+  render(){
+    return (
+      <div>
+        Add option
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render(){
+    return (
+      <div>
+                {this.props.optionText}
+      </div>
+    )
+  }
+}
 
 
-let template = <p>This is JSX from app.js</p>;
 
-let appRoot = document.getElementById('app');
-
-ReactDOM.render(template,appRoot);
+ReactDOM.render(<IndecisionApp/>,document.getElementById('app'));
