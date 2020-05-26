477ad27b2fc4069d0ee0aa32bae1c687479ce24f
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2020-05-26 12:50:54 -0700

    Added modal

---
+++ src/components/OptionModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const OptionModal = (props) =>  (
+  <Modal
+    isOpen={!!props.selectedOption}
+    onRequestClose={props.handleClearSelectedOption}
+    contentLabel="Selected Option"
+  >
+    <h3>
+      Selected Option
+    </h3>
+    {props.selectedOption && <p>Text</p>}
+    <button onClick={props.handleClearSelectedOption}>Okay</button>
+  </Modal>
+  )
+
+
+export default OptionModal;
